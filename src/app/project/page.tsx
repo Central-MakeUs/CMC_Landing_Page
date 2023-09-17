49352aea6342e402c618e4e84d08f691c875d1f7
hustle-dev <dlwoabsdk@gmail.com>
2023-09-17 19:36:23 +0900

    conf: next 정적 파일 내보내기 설정

---
+++ src/app/project/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'CMC Project',
+  description: 'CMC Project를 소개합니다.',
+}
+
+const ProjectPage = () => {
+  return <div>프로젝트</div>
+}
+
+export default ProjectPage
