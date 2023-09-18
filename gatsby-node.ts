a33c2f8599e596c6f52c85ed923bfd63bdc79cfc
hustle-dev <dlwoabsdk@gmail.com>
2023-09-18 19:51:04 +0900

    conf: gatsby 초기환경 설정

---
+++ gatsby-node.ts
@@ -0,0 +1,12 @@
+import type { CreateWebpackConfigArgs, GatsbyNode } from 'gatsby'
+import path from 'path'
+
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({ actions }: CreateWebpackConfigArgs) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        styles: path.resolve(__dirname, 'src/styles'),
+      },
+    },
+  })
+}
