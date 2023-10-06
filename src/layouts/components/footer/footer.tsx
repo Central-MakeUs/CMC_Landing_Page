30166940b780e5e3b87c4adb7aefb63a90f451c8
hustle-dev <dlwoabsdk@gmail.com>
2023-10-06 19:32:41 +0900

    feat: desktop용 footer 구현

--- src/layouts/components/footer/footer.tsx
+++ src/layouts/components/footer/footer.tsx
@@ -1,3 +1,18 @@
+import * as css from './footer.module.scss'
+import Instagram from './images/instagram.svg'
+import KakaoTalk from './images/kakaotalk.svg'
+
 export const Footer = () => {
-  return <footer>푸터</footer>
+  return (
+    <footer className={css.footer}>
+      <div className={css.info}>
+        <span className={css.name}>Central Makeus Challenge (CMC)</span>
+        <span className={css.copyright}>Copyrightⓒ 2023 All rights reserved by MakeUs Challenge</span>
+      </div>
+      <div className={css.contact}>
+        <Instagram />
+        <KakaoTalk />
+      </div>
+    </footer>
+  )
 }
