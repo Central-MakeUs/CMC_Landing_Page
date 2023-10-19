f82daaeab1ad78e5390de3fb0145363c4ccb4674
hustle-dev <hustle-dev@webtoonscorp.com>
2023-10-20 01:09:52 +0900

    feat: project 진행 중 중간커밋

---
+++ src/views/project/components/card/card.tsx
@@ -0,0 +1,5 @@
+import * as css from './card.module.scss'
+
+export const Card = () => {
+  return <figure className={css.card}>카드</figure>
+}
--- src/views/project/project.tsx
+++ src/views/project/project.tsx
@@ -1,5 +1,39 @@
+import { FloatingButton } from '@/components'
+
+import { Card } from './components'
+import * as css from './project.module.scss'
+
 const ProjectPage = () => {
-  return <div>프로젝트</div>
+  return (
+    <main className={css.main}>
+      <div className={css.background_header}>
+        <h2>CMC PROJECT</h2>
+        <div className={css.background_container}>
+          <div className={css.background} />
+        </div>
+      </div>
+      <nav className={css.nav}>
+        {/* TODO: 여기 폴더네이밍 데이터로 가져오게? */}
+        <button type="button" className={css.active}>
+          전체
+        </button>
+        <button type="button">10</button>
+        <button type="button">11</button>
+        <button type="button">12</button>
+        <button type="button">13</button>
+      </nav>
+      <div className={css.grid_container}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <FloatingButton />
+    </main>
+  )
 }
 
 export default ProjectPage
