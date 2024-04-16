28cb2f161910b8e971b3afb4de1dab8106a8bd79
Yeonju Jo <yjj0287@gmail.com>
2024-04-16 17:55:14 +0900

    design: 홈 화면 supports 섹션 제작

---
+++ src/views/home/components/supports-section/supports-section.tsx
@@ -0,0 +1,21 @@
+import { StaticImage } from 'gatsby-plugin-image'
+
+import { Section } from '../section'
+import * as css from './supports-section.module.scss'
+
+export const SupportsSection = () => {
+  return (
+    <Section>
+      <div className={css.space} />
+
+      <Section.Head title="Supports" />
+
+      <div className={css.supports}>
+        <StaticImage src="./images/kaist.png" alt="kaist" className={css.supports_company} />
+        <StaticImage src="./images/soft-squared.png" alt="soft squared" className={css.supports_company} />
+        <StaticImage src="./images/d-camp.png" alt="d camp" className={css.supports_company} />
+        <StaticImage src="./images/gridge.png" alt="gridge" className={css.supports_company} />
+      </div>
+    </Section>
+  )
+}
