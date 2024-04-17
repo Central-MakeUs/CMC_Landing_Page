bf9434424878cce7c3ea0d37ff676872a5e1c7f9
Yeonju Jo <yjj0287@gmail.com>
2024-04-18 03:14:05 +0900

    design: 공통 버튼 스타일 확장 가능하도록 수정

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,10 +1,20 @@
+import clsx from 'clsx'
 import { PropsWithChildren } from 'react'
 
 import * as css from './button.module.scss'
 
-export const Button = ({ children }: PropsWithChildren) => {
+interface Props extends PropsWithChildren {
+  className?: string
+}
+
+export const Button = ({ className, children }: Props) => {
   return (
-    <a href="https://forms.gle/LWfsozJbsrhkpyCr5" target="_blank" rel="noreferrer" className={css.button}>
+    <a
+      href="https://forms.gle/LWfsozJbsrhkpyCr5"
+      target="_blank"
+      rel="noreferrer"
+      className={clsx(css.button, className)}
+    >
       {children}
     </a>
   )
