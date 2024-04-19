bb66c35931d91edcf9784d8faf4f675d3bb907cd
Yeonju Jo <yjj0287@gmail.com>
2024-04-19 23:12:13 +0900

    chore: 후원사 목록 수정

--- src/views/home/components/supports-section/supports-section.tsx
+++ src/views/home/components/supports-section/supports-section.tsx
@@ -11,7 +11,6 @@ export const SupportsSection = () => {
       <Section.Head title="Supports" />
 
       <div className={css.supports}>
-        <StaticImage src="./images/kaist.png" alt="kaist" className={css.supports_company} />
         <StaticImage src="./images/soft-squared.png" alt="soft squared" className={css.supports_company} />
         <StaticImage src="./images/d-camp.png" alt="d camp" className={css.supports_company} />
         <StaticImage src="./images/gridge.png" alt="gridge" className={css.supports_company} />
