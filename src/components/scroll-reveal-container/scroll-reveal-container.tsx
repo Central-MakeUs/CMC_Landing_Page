5f6993ffcb22e3c840583e80c91458e2e47701f4
Yeonju Jo <yjj0287@gmail.com>
2024-05-15 13:52:33 +0900

    chore: 사용법 주석 추가

--- src/components/scroll-reveal-container/scroll-reveal-container.tsx
+++ src/components/scroll-reveal-container/scroll-reveal-container.tsx
@@ -7,6 +7,21 @@ interface Props extends PropsWithChildren {
   className?: string
 }
 
+/**
+ * 스크롤이 해당 역영에 위치했을 때, 위로 올라오면서 등장하는 효과를 주는 컴포넌트
+ *
+ * before: opacity 0, translateY: 50px
+ *  -> after: opacity: 1, translateY: 0px
+ *
+ * @component
+ * @param {string} children 효과를 줄 자식 컴포넌트
+ * @param {string} className 원하는 class가 있다면 추가할 수 있습니다.
+ *
+ * @example
+ * <ScrollRevealContainer> // 효과를 줄 컴포넌트를 감싸주면 됩니다.
+ *   <ExampleSection />
+ * </ScrollRevealContainer>
+ */
 export const ScrollRevealContainer = ({ className = '', children }: Props) => {
   useEffect(() => {
     const initObserver = () => {
