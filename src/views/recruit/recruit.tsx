95072e326b771fabd6eb5bbb5828f235a2f66e9a
Paul Yang <sea5122916@gmail.com>
2025-04-12 10:53:04 +0900

    recruit 페이지

--- src/views/recruit/recruit.tsx
+++ src/views/recruit/recruit.tsx
@@ -1,9 +1,10 @@
 import { HeadProps } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 
-import {  ScrollRevealContainer, Section, Seo } from '@/components'
+import { ScrollRevealContainer, Section, Seo } from '@/components'
 import { Main } from '@/layouts'
 
-import { AuraEffect, PlanCard } from './components'
+import { PlanCard } from './components'
 import { RecruitPlans, SessionPlans } from './constants'
 import * as css from './recruit.module.scss'
 
@@ -11,13 +12,24 @@ const RecruitPage = () => {
   return (
     <Main className={css.main}>
       <ScrollRevealContainer>
+        <div className={css.background}>
+          <StaticImage src="../../images/recruit_background.png" alt="" className={css.desktop} />
+          <StaticImage src="../../images/recruit_background_mobile.png" alt="" className={css.mobile} />
+        </div>
         <Section className={css.section}>
-        
-          <AuraEffect />
+          {/* <AuraEffect /> */}
 
           <Section.Head title="Join Us!" description="16기 일정을 소개합니다" />
-          <PlanCard title="CMC 합류 여정" plans={RecruitPlans} showButton />
-          <PlanCard title="CMC 정기 세션" plans={SessionPlans} />
+          <div className={css.schedule}>
+            <div className={css.bg1}>
+              <StaticImage src="../../images/recruit_bg.png" alt="" />
+            </div>
+            <div className={css.bg2}>
+              <StaticImage src="../../images/recruit_bg2.png" alt="" />
+            </div>
+            <PlanCard title="CMC 합류 여정" plans={RecruitPlans} showButton />
+            <PlanCard title="CMC 정기 세션" plans={SessionPlans} />
+          </div>
           <div className={css.space} />
         </Section>
       </ScrollRevealContainer>
