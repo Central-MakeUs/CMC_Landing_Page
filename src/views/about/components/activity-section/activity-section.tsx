416dedb1f7b8806fae4242deaf3110c21d653e75
JJHyeok13 <jjinh99@naver.com>
2025-04-14 00:16:25 +0900

    fix: 정기활동 부분 재수정

--- src/views/about/components/activity-section/activity-section.tsx
+++ src/views/about/components/activity-section/activity-section.tsx
@@ -36,63 +36,61 @@ export const ActivitySection = () => {
       {/* 모바일용 배경 이미지 */}
       <StaticImage className={css.backgroundImgMobile} src="./images/background_mobile.png" alt="이미지 설명" />
 
-      <div className={css.container}>
-        <Section.Head title="매 기수 진행되는 정기 활동들" white />
+      <Section.Head title="매 기수 진행되는 정기 활동들" white />
 
-        <div className={css.cardContainer}>
-          <StaticImage className={css.fireImage} src="./images/fire.png" alt="fire" />
-          <StaticImage className={css.labtopImage} src="./images/labtop.png" alt="labtop" />
-          <Card
-            chapter="CHAPTER.1"
-            title="UMC와 연합 해커톤"
-            image={<StaticImage className={css.image} src="./images/img1.png" />}
-            description={
-              <>
-                CMC 정기세션 이전 진행되는 세션으로 단시간 내 IT 프로덕트를 <Br desktop />
-                설계하고, 구현하는 경험을 가집니다.
-              </>
-            }
-          />
+      <div className={css.cardContainer}>
+        <StaticImage className={css.fireImage} src="./images/fire.png" alt="fire" />
+        <StaticImage className={css.labtopImage} src="./images/labtop.png" alt="labtop" />
+        <Card
+          chapter="CHAPTER.1"
+          title="UMC와 연합 해커톤"
+          image={<StaticImage className={css.image} src="./images/img1.png" />}
+          description={
+            <>
+              CMC 정기세션 이전 진행되는 세션으로 단시간 내 IT 프로덕트를 <Br desktop />
+              설계하고, 구현하는 경험을 가집니다.
+            </>
+          }
+        />
 
-          <Card
-            chapter="CHAPTER.2"
-            title="모각작"
-            image={<StaticImage className={css.image} src="./images/img2.png" />}
-            description={
-              <>
-                모든 파트가 함께 모여 현재까지의 프로젝트 진척도를 점검합니다. <Br desktop />
-                또한, 챌린저 간 진행되는 UT를 통해 프로덕트 사용성을 객관적으로 <Br desktop />
-                평가하고 이를 반영하여 보완합니다.
-              </>
-            }
-          />
+        <Card
+          chapter="CHAPTER.2"
+          title="모각작"
+          image={<StaticImage className={css.image} src="./images/img2.png" />}
+          description={
+            <>
+              모든 파트가 함께 모여 현재까지의 프로젝트 진척도를 점검합니다. <Br desktop />
+              또한, 챌린저 간 진행되는 UT를 통해 프로덕트 사용성을 객관적으로 <Br desktop />
+              평가하고 이를 반영하여 보완합니다.
+            </>
+          }
+        />
 
-          <Card
-            chapter="CHAPTER.3"
-            title="런칭데이"
-            image={<StaticImage className={css.image} src="./images/img3.png" />}
-            description={
-              <>
-                데모데이 개최 이전에 CMC 전체 챌린저들을 대상으로 런칭된 프로덕트를 <Br desktop />
-                소개합니다. 해당 세션을 통해 프로덕트 구현 과정과 추후 프로덕트 <Br desktop />
-                방향성에 대해 공유합니다.
-              </>
-            }
-          />
+        <Card
+          chapter="CHAPTER.3"
+          title="런칭데이"
+          image={<StaticImage className={css.image} src="./images/img3.png" />}
+          description={
+            <>
+              데모데이 개최 이전에 CMC 전체 챌린저들을 대상으로 런칭된 프로덕트를 <Br desktop />
+              소개합니다. 해당 세션을 통해 프로덕트 구현 과정과 추후 프로덕트 <Br desktop />
+              방향성에 대해 공유합니다.
+            </>
+          }
+        />
 
-          <Card
-            chapter="CHAPTER.4"
-            title="데모데이"
-            image={<StaticImage className={css.image} src="./images/img4.png" />}
-            description={
-              <>
-                VC와 일반 참가자들을 대상으로 부스를 운영하며 지금까지 제작한 <Br desktop />
-                프로덕트를 소개하고 홍보합니다. 데모데이를 통해 초기 사용자를 <Br desktop />
-                모집하고, 런칭된 프로덕트의 시장성을 검증합니다.
-              </>
-            }
-          />
-        </div>
+        <Card
+          chapter="CHAPTER.4"
+          title="데모데이"
+          image={<StaticImage className={css.image} src="./images/img4.png" />}
+          description={
+            <>
+              VC와 일반 참가자들을 대상으로 부스를 운영하며 지금까지 제작한 <Br desktop />
+              프로덕트를 소개하고 홍보합니다. 데모데이를 통해 초기 사용자를 <Br desktop />
+              모집하고, 런칭된 프로덕트의 시장성을 검증합니다.
+            </>
+          }
+        />
       </div>
     </Section>
   )
