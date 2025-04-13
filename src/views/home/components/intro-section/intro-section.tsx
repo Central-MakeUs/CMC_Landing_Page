36b9fe43a50f3f8aedf6c78905af709d371cc0b9
JJHyeok13 <jjinh99@naver.com>
2025-04-14 01:20:26 +0900

    fix: QA 진행

--- src/views/home/components/intro-section/intro-section.tsx
+++ src/views/home/components/intro-section/intro-section.tsx
@@ -6,7 +6,7 @@ import * as css from './intro-section.module.scss'
 export const IntroSection = () => {
   return (
     <section className={css.intro}>
-      <StaticImage className={css.backgroundImg} src="../../images/main.png" alt="main" />
+      <StaticImage className={css.backgroundImg} src="../../images/kv_1440_2x.png" alt="main" />
 
       <StaticImage className={css.backgroundImgMobile} src="../../images/main_mobile.png" alt="main" />
     </section>
