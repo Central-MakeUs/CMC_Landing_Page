df912904b72915dd82b5a494e0dedacdece99c24
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2025-04-25 15:13:31 +0900

    fix: 지원기간 아닙니다 제거

--- src/views/about/components/apply-section/apply-section.tsx
+++ src/views/about/components/apply-section/apply-section.tsx
@@ -6,10 +6,10 @@ export const ApplySection = () => {
   return (
     <Section>
       <Section.Head title="CMC와 함께 도전하세요" />
-      <p className={css.alert}>
+{/*       <p className={css.alert}>
         현재 지원 기간이 아닙니다. 다음 기수 모집이 <Br mobile />
         시작될 때까지 조금만 기다려 주세요.
-      </p>
+      </p> */}
       <ApplyButton className={css.button}>
         CMC 17기 지원 알림받기
         <div className={css.button_effect} />
