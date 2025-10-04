d7cc806b6dffa76c0235d3743e69abf676ae213c
YoungCheon <kyt031522@gmail.com>
2025-10-04 22:17:40 +0900

    🐛 Kakao 링크 삭제

--- src/views/faq/faq.tsx
+++ src/views/faq/faq.tsx
@@ -3,7 +3,7 @@ import { HeadProps } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import { useState } from 'react'
 
-import { ScrollRevealContainer, Section, Seo, AuraEffect } from '@/components'
+import { AuraEffect,ScrollRevealContainer, Section, Seo } from '@/components'
 import { Main } from '@/layouts'
 
 import { Cards, Tab } from './constants'
@@ -30,7 +30,6 @@ const FaqPage = () => {
     <Main className={css.main}>
       <ScrollRevealContainer>
         <Section className={css.section}>
-
           <AuraEffect />
 
           <Section.Head title="FAQ" />
@@ -69,7 +68,6 @@ const FaqPage = () => {
                 alt="CMC 인스타 스토리 확인하러 가는 버튼"
                 className={css.sns_icon}
               />
-              
               <StaticImage
                 src="./images/sns-insta-blue.png"
                 alt="CMC 인스타 스토리 확인하러 가는 버튼"
@@ -77,11 +75,11 @@ const FaqPage = () => {
               />
               CMC 인스타 스토리 확인
             </a>
-            <a href="http://pf.kakao.com/_xcwDJT" target="_blank" rel="noreferrer" className={css.sns_button}>
+            {/* <a href="http://pf.kakao.com/_xcwDJT" target="_blank" rel="noreferrer" className={css.sns_button}>
               <StaticImage src="./images/sns-kakao.png" alt="CMC 카카오톡 문의하기 버튼" className={css.sns_icon_kakao} />
               <StaticImage src="./images/sns-kakao-blue.png" alt="CMC 카카오톡 문의하기 버튼" className={css.sns_icon_kakao_mobile} objectFit='contain'/>
               카카오톡 문의
-            </a>
+            </a> */}
           </div>
 
           <div className={css.space} />
