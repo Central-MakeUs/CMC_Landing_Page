1dcb3f18d2bb2139f834a14b1f79b4d62f6b68c9
yummjin <cho4u4o@gmail.com>
2026-04-02 15:52:04 +0900

    refactor: entrysection 정보 중앙화

---
+++ src/constants/home/entrySection.ts
@@ -0,0 +1,19 @@
+export const ENTRY_SECTION_DATA = {
+  mainImage: {
+    alt: 'home main',
+    image: '/images/home-main.svg',
+  },
+  text1: {
+    alt: 'text 1',
+    image: '/images/home-text-1.svg',
+  },
+  text2: {
+    alt: 'text 2',
+    image: '/images/home-text-2.svg',
+  },
+  text3: {
+    alt: 'text 3',
+    image: '/images/home-text-3.svg',
+    pressedImage: '/images/home-text-3-pressed.svg',
+  },
+}
