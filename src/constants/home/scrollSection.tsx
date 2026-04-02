79c3b76a61140a8e07f2bcbc1ea69f6b04020b40
yummjin <cho4u4o@gmail.com>
2026-04-02 16:08:16 +0900

    feat: 홈 스크롤 컨텐츠 및 인터랙션 구현

---
+++ src/constants/home/scrollSection.tsx
@@ -0,0 +1,29 @@
+export const SCROLL_SECTION_DATA = {
+  star: {
+    alt: 'star',
+    image: '/images/home-star.svg',
+  },
+  title: 'We are CMC',
+  text1: {
+    text: (
+      <>
+        짧은 시간 내에
+        <br />
+        시장성을 검증해서 출시하고,
+      </>
+    ),
+    alt: 'graphic 1',
+    image: '/images/home-scroll-gp-1.svg',
+  },
+  text2: {
+    text: (
+      <>
+        &apos;수익형&apos; &apos;앱&apos;
+        <br />
+        &apos;개발&apos;을 하는 동아리예요.
+      </>
+    ),
+    alt: 'graphic 2',
+    image: '/images/home-scroll-gp-2.svg',
+  },
+}
---
+++ src/hooks/home/useHomeScrollAnimation.ts
@@ -0,0 +1,49 @@
+import { useRef } from 'react'
+import { useScroll, useTransform } from 'motion/react'
+
+const STAR_SIZE = 100
+const BP1 = 200
+const BP2 = 400
+const BP3 = 600
+const BP4 = 800
+
+export function useHomeScrollAnimation(scrollContainerRef: React.RefObject<HTMLDivElement | null>) {
+  const sectionRef = useRef<HTMLElement>(null)
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    container: scrollContainerRef,
+    offset: ['start start', 'end end'],
+  })
+
+  const lineHeight = useTransform(scrollYProgress, [0, 0.25, 0.5, 0.75, 1.0], [0, BP1, BP2, BP3, BP4])
+
+  const starY = useTransform(lineHeight, (h) => Math.max(0, h - STAR_SIZE / 2))
+  const starOpacity = useTransform(lineHeight, [0, BP1 * 0.15], [0, 1])
+
+  const weCMCY = useTransform(lineHeight, (h) => {
+    if (typeof window === 'undefined') return 0
+    return Math.min(h + STAR_SIZE / 2 + 24, window.innerHeight * 0.75)
+  })
+  const weCMCOpacity = useTransform(lineHeight, [BP1 * 0.4, BP1, BP2 * 0.85, BP2], [0, 1, 1, 0])
+
+  const gp1Opacity = useTransform(lineHeight, [BP2 * 0.9, BP2, BP3 * 0.85, BP3], [0, 1, 1, 0])
+  const text1Opacity = useTransform(lineHeight, [BP2, BP2 * 1.1, BP3 * 0.85, BP3], [0, 1, 1, 0])
+
+  const gp2Opacity = useTransform(lineHeight, [BP3 * 0.9, BP3, BP4 * 0.85, BP4], [0, 1, 1, 1])
+  const text2Opacity = useTransform(lineHeight, [BP3, BP3 * 1.07, BP4 * 0.85, BP4], [0, 1, 1, 1])
+
+  return {
+    sectionRef,
+    lineHeight,
+    starY,
+    starOpacity,
+    weCMCY,
+    weCMCOpacity,
+    gp1Opacity,
+    text1Opacity,
+    gp2Opacity,
+    text2Opacity,
+  }
+}
+
