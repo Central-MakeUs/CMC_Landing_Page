52f8deda3fcc555ffc2937315acbf265523a3595
sandwe <preengus@gmail.com>
2026-04-09 01:06:02 +0900

    feat: 푸터 반응형 작업

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,33 +5,33 @@ import { INSTAGRAM_URL, KAKAO_CHANNEL_URL } from '@/constants/links'
 export default function Footer() {
   return (
     <footer className="w-full bg-black">
-      <div className="mx-auto flex max-w-[1920px] flex-col gap-4 px-6 py-12 md:min-h-[191px] md:flex-row md:items-center md:justify-between md:px-[200px] md:pb-[57px] md:pt-[58px] md:flex-wrap">
+      <div className="mx-auto flex max-w-[1920px] flex-row items-center justify-between gap-4 px-5 py-[22px] md:px-6 md:min-h-[191px] xl:px-[200px] md:pb-[57px] md:pt-[58px]">
         <div className="flex flex-col gap-[10px]">
-          <Image src="/logo-dark.svg" width={30} height={30} alt="" className="h-[30px] w-[30px]" />
-          <div className="text-[13px] font-light leading-[18px] tracking-[-0.26px] text-gray-700">
+          <Image src="/logo-dark.svg" width={30} height={30} alt="" className="md:h-[30px] md:w-[30px] w-6 h-6" />
+          <div className="text-[10px] font-light leading-[16px] tracking-[-0.2px] text-gray-700 md:text-[13px] md:leading-[18px] md:tracking-[-0.26px]">
             <p className="mb-0">Central Makeus Challenge (CMC)</p>
             <p>Copyrightⓒ 2026 All rights reserved by MakeUs Challenge</p>
           </div>
         </div>
 
-        <div className="flex items-center gap-[26px]">
+        <div className="flex shrink-0 items-center gap-5 md:gap-[26px]">
           <a
             href={KAKAO_CHANNEL_URL}
             target="_blank"
             rel="noopener noreferrer"
             aria-label="카카오톡 채널"
-            className="flex size-[48px] shrink-0 items-center justify-center"
+            className="flex shrink-0 items-center justify-center"
           >
-            <Image src="/images/footer-kakao.svg" width={44} height={41} alt="" />
+            <Image src="/images/footer-kakao.svg" width={44} height={44} alt="" className="md:w-11 md:h-11 w-6 h-6" />
           </a>
           <a
             href={INSTAGRAM_URL}
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Instagram"
-            className="flex size-[48px] shrink-0 items-center justify-center"
+            className="flex shrink-0 items-center justify-center"
           >
-            <Image src="/images/footer-insta.svg" width={44} height={44} alt="" />
+            <Image src="/images/footer-insta.svg" width={44} height={44} alt="" className="md:w-11 md:h-11 w-6 h-6" />
           </a>
         </div>
       </div>
