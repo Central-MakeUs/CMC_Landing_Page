d5ee5f5704328815431bef6ef81d1a1801f038c1
sandwe <preengus@gmail.com>
2026-04-16 21:51:42 +0900

    feat: 19기 사전예약으로 변경

--- src/constants/apply.ts
+++ src/constants/apply.ts
@@ -1,10 +1,10 @@
 /** 모집 마감 날짜 (KST) */
-export const APPLY_DEADLINE = new Date('2026-05-01T00:00:00+09:00')
+export const APPLY_DEADLINE = new Date('2025-10-29T00:00:00+09:00')
 
 export const APPLY_DATA = {
   title: 'Join to CMC',
   getButtonHref: (isEnded: boolean) =>
-    isEnded ? 'https://forms.gle/2sqqazVFLNV7MYan7' : 'https://forms.gle/HL54afqxkvTN5QiB8',
+    isEnded ? 'https://forms.gle/2sqqazVFLNV7MYan7' : 'https://forms.gle/6uMPJyquobwUkTcu5',
   countdown: {
     days: 'DAYS',
     hours: 'HOURS',
