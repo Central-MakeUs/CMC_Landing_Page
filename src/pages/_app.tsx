10c0070dd7c5fbd3fd24386974f945ab1784bb8b
sandwe <preengus@gmail.com>
2026-04-16 23:21:55 +0900

    feat: ga 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { yapariTrial } from '@/styles/font'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={yapariTrial.variable}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <GoogleAnalytics gaId="G-N6KW5CY5YH" />
+      <div className={yapariTrial.variable}>
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
