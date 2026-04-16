92f7dfe47886ca5fb6c54c66e5a0a20445300d82
sandwe <preengus@gmail.com>
2026-04-17 01:19:53 +0900

    feat: favicon 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
         <link rel="preconnect" href="https://cdn.jsdelivr.net" />
