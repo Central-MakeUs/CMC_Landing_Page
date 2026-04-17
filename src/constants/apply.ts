7a64bfc0319d829bab86229cefa546e675de0d31
sandwe <preengus@gmail.com>
2026-04-17 23:29:36 +0900

    feat: 기준 기수 19기로 변경

--- src/constants/apply.ts
+++ src/constants/apply.ts
@@ -1,5 +1,5 @@
 /** 모집 마감 날짜 (KST) */
-export const APPLY_DEADLINE = new Date('2025-10-29T00:00:00+09:00')
+export const APPLY_DEADLINE = new Date('2026-04-25T00:00:00+09:00')
 
 export const isApplyEnded = () => Date.now() >= APPLY_DEADLINE.getTime()
 
@@ -7,8 +7,8 @@ export const APPLY_DATA = {
   title: 'Join to CMC',
   getButtonHref: (isEnded: boolean) =>
     isEnded
-      ? 'https://docs.google.com/forms/d/e/1FAIpQLScFd4ILrh_tHyg7N5bkc-A4uLxI9CRseSs9ziEWWSmRy48WXw/viewform?usp=header'
-      : 'https://forms.gle/6uMPJyquobwUkTcu5',
+      ? 'https://docs.google.com/forms/d/e/1FAIpQLSdujjhpMlmTp-T-wtREZwiymSCA7xoO0Z_J1CnaXx3edssI-Q/viewform'
+      : 'https://docs.google.com/forms/d/e/1FAIpQLSdUv7yki5QZsDsElhTx-3em48Li_a89g4OWLP3jMNGm53ibWg/viewform?usp=dialog',
   countdown: {
     days: 'DAYS',
     hours: 'HOURS',
